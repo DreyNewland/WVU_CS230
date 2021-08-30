ab67d371111fb5eee2cc62f312991df9db78a547
DreyNewland <dan0016@mix.wvu.edu>
2021-08-30 18:12:54 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { TopNavBarComponent2 } from './header/top-nav-bar2.component';
+import { TopNavBarComponent3 } from './header/top-nav-bar3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    TopNavBarComponent2,
+    TopNavBarComponent3
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/top-nav-bar3.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-top-nav-bar3',
+    templateUrl: 'top-nav-bar3.component.html'
+})
+export class TopNavBarComponent3 {
+
+}
